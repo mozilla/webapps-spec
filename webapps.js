4bb93d56be4e49d3788360b6b273814ad45e9fc5
Anant Narayanan <anant@kix.in>
2012-07-20 15:28:17 -0700

    Update to latest respec and cleanup

---
+++ webapps.js
@@ -0,0 +1,17 @@
+var respecConfig = {
+    previousURI: "-",
+    specStatus: "ED",
+    shortName: "webapps-spec",
+    subtitle: "A format for describing web applications and APIs to install, launch and manage them",
+    edDraftURI: "http://mozilla.github.com/webapps-spec/",
+    extraCSS: ["http://dev.w3.org/2009/dap/ReSpec.js/css/respec.css"],
+
+    editors:  [
+        { name: "Anant Narayanan", company: "Mozilla" }
+    ],
+
+    wg:           "Webapps Working Group",
+    wgURI:        "http://www.w3.org/2008/webapps/",
+    wgPublicList: "public-webapps",
+    wgPatentURI:  "http://www.w3.org/2004/01/pp-impl/42538/status"
+};
